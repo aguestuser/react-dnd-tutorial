30bb4d0db0679c3c7178956cef1fce21eaf860f1
aguestuser <aguestuser@riseup.net>
2016-09-07 14:51:54 -0400

    Add functionality to click on square to move knight there (if legal move)

--- src/component/Board.js
+++ src/component/Board.js
@@ -3,6 +3,7 @@ import Square from './Square';
 import Knight from './Knight';
 import NoPiece from './NoPiece';
 import {range} from 'lodash';
+import {maybeMoveKnight} from "../state/Game";
 
 export default class Board extends Component {
   render(){
@@ -24,15 +25,23 @@ export default class Board extends Component {
   }
 }
 
+Board.PropTypes = {
+  knightPosition: PropTypes.arrayOf(
+    PropTypes.number.isRequired
+  ).isRequired
+};
+
 // (Int,Int) -> (Int) -> Component
 const renderSquareOf = (kx,ky) => (i) => {
+
   const [x,y] = [i % 8, Math.floor(i / 8)];
   const black = (x + y) % 2 === 1;
   const piece = (x === kx && y === ky) ? <Knight/> : <NoPiece/>;
 
   return (
     <div key={i}
-         style={{width: '12.5%', height: '12.5%'}}>
+         style={{width: '12.5%', height: '12.5%'}}
+         onClick={() => handleSquareClick(x,y)}>
       <Square black={black}>
         {piece}
       </Square>
@@ -40,9 +49,5 @@ const renderSquareOf = (kx,ky) => (i) => {
   );
 };
 
-
-Board.PropTypes = {
-  knightPosition: PropTypes.arrayOf(
-    PropTypes.number.isRequired
-  ).isRequired
-};
+// (Int,Int) => Unit
+const handleSquareClick = (toX,toY) => maybeMoveKnight(toX,toY);
--- src/state/Game.js
+++ src/state/Game.js
@@ -1,5 +1,28 @@
-export const observe = (receive) =>
-  setInterval(() => receive([
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  ]), 500);
+let knightPosition = [0,0];
+let observer = null;
+
+const emitChange = () => observer(knightPosition);
+
+export const observe = (o) => {
+  if (observer) throw new Error('Multiple observers not implemented.');
+  observer = o;
+  emitChange();
+};
+
+export const canMoveKnight = (toX,toY) => {
+  const [x,y] = knightPosition;
+  const dx = toX - x;
+  const dy = toY - y;
+
+  return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+         (Math.abs(dx) === 1 && Math.abs(dy) === 2);
+};
+
+export const moveKnight = (toX, toY) => {
+  knightPosition = [toX,toY];
+  emitChange();
+};
+
+export const maybeMoveKnight = (toX,toY) => {
+  if (canMoveKnight(toX,toY)) moveKnight(toX,toY);
+};
