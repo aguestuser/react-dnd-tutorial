f8653cb1ba8e9da798c59fff758c989fef8227c1
aguestuser <aguestuser@riseup.net>
2016-09-07 17:34:55 -0400

    Cleanup Square component (extract styles, use static propType declaration)

--- src/component/Square.js
+++ src/component/Square.js
@@ -1,24 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Square extends Component {
-  render(){
-    const {black} = this.props;
-    const style = {
-      backgroundColor: black ? 'black' : 'white',
-      color: black ? 'white' : 'black',
-      width: '100%',
-      height: '100%',
-      flexGrow: 1
-    };
 
+  static proptypes = {
+    black: PropTypes.bool
+  };
+
+  render(){
     return (
-      <div style={style}>
+      <div style={style(this.props.black)}>
         {this.props.children}
       </div>
     )
   }
 }
 
-Square.propTypes = {
-  black: PropTypes.bool
-};
+const style = (black) => ({
+  backgroundColor: black ? 'black' : 'white',
+  color: black ? 'white' : 'black',
+  width: '100%',
+  height: '100%',
+  flexGrow: 1
+});
